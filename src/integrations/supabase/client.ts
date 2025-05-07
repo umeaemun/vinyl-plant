8a03d322358c386ea8ad50055a30430f9a82ee67
ume aemun <umeaemun@gmail.com>
2025-05-07 23:20:08 +0100

    changes

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -4,4 +4,11 @@ import type { Database } from './types';
 
 
 
-export const supabase = createClient<Database>(process.env.SUPABASE_URL, process.env.SUPABASE_PUBLISHABLE_KEY);
+export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
+
+
+
+
+
+
